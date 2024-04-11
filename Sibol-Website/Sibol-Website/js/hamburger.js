c2bb092f6ecd43f83ef48d2b83d11c8e327812ef
Elidia <51367191+nicorobin7@users.noreply.github.com>
2024-04-11 10:26:48 +0800

    Update hamburger.js

--- Sibol-Website/Sibol-Website/js/hamburger.js
+++ Sibol-Website/Sibol-Website/js/hamburger.js
@@ -1,29 +1,25 @@
-// function showNavbar() {
-//     const myNav = document.getElementById("nav-list");
-//     if (myNav.style.display === "block") {
-//       myNav.style.display = "none";
-//     } else {
-//       myNav.style.display = "block";
-//     }
-//   }
-
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector("#nav-list");
+const navList = document.querySelector("#nav-list");
+const navMenu = document.querySelector(".nav-tabs");
 const body = document.querySelector("body");
 const windowWidth = document.querySelector("body").style.width;
-
-function closeNav() {
-    if (windowWidth >= "768px" ) {
-        mobileMenu();
-    }
-}
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
 
     hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+    navList.classList.toggle("active");
     body.classList.toggle("active");
 
 }
+
+navList.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navList.classList.remove("active");
+    body.classList.remove("active");
+}
+
+
